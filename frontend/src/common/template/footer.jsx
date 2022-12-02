36c59b395011f16b8c52fb4b842503456d3b88ea
Marcelo Dias <diasteixeira1990@gmail.com>
2022-12-02 10:12:14 -0300

    criado Menu da aplicação

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2022
+            <a href="https://www.linkedin.com/in/marcelo-dias-b098ab229/" target='_blank'> Marcelo Dias</a>
+        </strong>
+    </footer>
+)
